4ce03da0d8de75e483e546c879c7aa3c5de09818
spbills <spencebills0@gmail.com>
2023-01-24 17:51:54 -0500

    Added photos to sidebar

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
+    let photos: Array<{ url: string, title: string }> = [];
+
     onMount(() => {
         // Listen for messages from the extension
         window.addEventListener("message", (event) => {
@@ -12,7 +14,15 @@
                 }
             }
         });
+
+        fetch(`https://jsonplaceholder.typicode.com/photos`)
+            .then(d => d.json())
+            .then(j => photos = j.slice(1).slice(-5));
     });
 </script>
 
 <h1>Sidebar Panel</h1>
+
+{#each photos as photo}
+    <img src={photo.url} width=150 alt={photo.title} />
+{/each}
